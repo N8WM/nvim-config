ac36d31537393a3fc26809ed080127d2c6efabab
Nathan McCutchen <n8wm@icloud.com>
2023-12-29 16:54:32 -0800

    formatting and linting

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,7 +1,11 @@
 return {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
-    opts = { -- this is equalent to setup({}) function
-        check_ts = true
-    }
+    opts = {},
+    config = function()
+        require("nvim-autopairs").setup({
+            disable_filetype = { "TelescopePrompt" },
+            check_ts = true
+        })
+    end
 }
--- lua/plugins/lsp_config.lua
+++ lua/plugins/lsp_config.lua
@@ -1,42 +1,61 @@
 local on_attach = function(_, _)
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, {})
-    vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
+    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
 
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-    vim.keymap.set('n', 'gr', require('telescope.builtin').lsp_references, {})
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {})
+    vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, {})
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 end
 
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
-
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
 return {
     {
         "williamboman/mason.nvim",
         config = function()
             require("mason").setup()
-        end
+        end,
     },
     {
         "williamboman/mason-lspconfig.nvim",
         config = function()
             require("mason-lspconfig").setup({
                 ensure_installed = {
-                    "angularls", "arduino_language_server", "asm_lsp", "bashls", "clangd",
-                    "omnisharp", "cmake", "cssls", "dockerls", "eslint", "fortls", "html",
-                    "htmx", "jsonls", "tsserver", "texlab", "lua_ls", "marksman", "pyright",
-                    "rust_analyzer", "vimls", "lemminx", "yamlls", "jdtls"
-                }
+                    "angularls",
+                    "arduino_language_server",
+                    "asm_lsp",
+                    "bashls",
+                    "clangd",
+                    "omnisharp",
+                    "cmake",
+                    "cssls",
+                    "dockerls",
+                    "eslint",
+                    "fortls",
+                    "html",
+                    "htmx",
+                    "jsonls",
+                    "tsserver",
+                    "texlab",
+                    "lua_ls",
+                    "marksman",
+                    "pyright",
+                    "rust_analyzer",
+                    "vimls",
+                    "lemminx",
+                    "yamlls",
+                    "jdtls",
+                },
             })
             require("mason-lspconfig").setup_handlers({
                 -- The first entry (without a key) will be the default handler
                 -- and will be called for each installed server that doesn't have
                 -- a dedicated handler.
-                function (server_name) -- default handler (optional)
+                function(server_name) -- default handler (optional)
                     require("lspconfig")[server_name].setup({
                         on_attach = on_attach,
-                        capabilities = capabilities
+                        capabilities = capabilities,
                     })
                 end,
 
@@ -45,9 +64,43 @@ return {
                 --     lspconfig.lua_ls.setup({...})
                 -- end
             })
-        end
+        end,
+    },
+    { "neovim/nvim-lspconfig" },
+    {
+        "nvimtools/none-ls.nvim",
+        config = function()
+            vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+        end,
     },
     {
-        "neovim/nvim-lspconfig"
-    }
+        "jay-babu/mason-null-ls.nvim",
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "williamboman/mason.nvim",
+            "nvimtools/none-ls.nvim",
+        },
+
+        config = function()
+            local null_ls = require("null-ls")
+            require("mason-null-ls").setup({
+                ensure_installed = {
+                    -- formatting
+                    "stylua",
+                    "prettierd",
+                    "isort",
+                    "black",
+                    -- diagnostics
+                    "eslint_d",
+                },
+                automatic_installation = true,
+                handlers = {},
+            })
+            null_ls.setup({
+                sources = {
+                    -- Anything not supported by mason.
+                },
+            })
+        end,
+    },
 }
