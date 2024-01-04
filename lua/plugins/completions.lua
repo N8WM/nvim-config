4ce1acf78b72f398ef03e5cbcd0a257c071e0169
Nathan McCutchen <n8wm@icloud.com>
2024-01-04 15:50:28 -0800

    cmp suggestion sorting

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -13,6 +13,7 @@ return {
 		"hrsh7th/nvim-cmp",
 		config = function()
 			local cmp = require("cmp")
+            local compare = require("cmp.config.compare")
 			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
@@ -33,11 +34,36 @@ return {
 					["<CR>"] = cmp.mapping.confirm({ select = true }),
 				}),
 				sources = cmp.config.sources({
-					{ name = "nvim_lsp" },
-					{ name = "luasnip" }, -- For luasnip users.
-				}, {
-					{ name = "buffer" },
+					-- { name = "nvim_lsp_signature_help" },
+					{ name = "cmp_tabnine", priority = 8 },
+					{ name = "nvim_lsp", priority = 8 },
+					{ name = "luasnip", priority = 7 },
+					{ name = "buffer", priority = 7 }, -- first for locality sorting?
+					{ name = "spell", keyword_length = 3, priority = 5, keyword_pattern = [[\w\+]] },
+					{ name = "dictionary", keyword_length = 3, priority = 5, keyword_pattern = [[\w\+]] }, -- from uga-rosa/cmp-dictionary plug
+					-- { name = 'rg'},
+					{ name = "nvim_lua", priority = 5 },
+					{ name = "path" },
+					{ name = "fuzzy_path", priority = 4 }, -- from tzacher
+					{ name = "calc", priority = 3 },
+					-- { name = 'vsnip' },
 				}),
+				sorting = {
+					priority_weight = 1.0,
+					comparators = {
+						-- compare.score_offset, -- not good at all
+						compare.locality,
+						compare.recently_used,
+						compare.score, -- based on :  score = score + ((#sources - (source_index - 1)) * sorting.priority_weight)
+						compare.offset,
+						compare.order,
+						-- compare.scopes, -- what?
+						-- compare.sort_text,
+						-- compare.exact,
+						-- compare.kind,
+						-- compare.length, -- useless
+					},
+				},
 			})
 		end,
 	},
