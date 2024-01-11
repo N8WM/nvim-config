7b25dba2fca231552d58999432de2c141d93fbff
Nathan McCutchen <n8wm@icloud.com>
2024-01-10 18:51:05 -0800

    cursor

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
 -- Set options
-vim.o.filetype = 'on'
-vim.o.syntax = 'on'
+vim.o.filetype = "on"
+vim.o.syntax = "on"
 vim.o.cursorline = true
 vim.o.expandtab = true
 vim.o.tabstop = 4
@@ -17,61 +17,59 @@ vim.o.showcmd = true
 vim.o.showmode = true
 vim.o.scrolloff = 1
 vim.o.wildmenu = true
-vim.o.wildmode = 'list:longest'
-vim.o.wildignore = '*.docx,*.jpg,*.png,*.gif,*.pdf,*.pyc,*.exe,*.flv,*.img,*.xlsx'
+vim.o.wildmode = "list:longest"
+vim.o.wildignore = "*.docx,*.jpg,*.png,*.gif,*.pdf,*.pyc,*.exe,*.flv,*.img,*.xlsx"
 
 vim.o.pumheight = 10
 vim.o.pumblend = 20
 vim.o.winblend = 10
-
+vim.o.guicursor = "i:block-blinkon1"
 vim.o.textwidth = 80
 
-
 -- Set autocommands
-vim.api.nvim_create_autocmd('FileType', {
-    pattern = {'css', 'html', 'javascript', 'tex', 'yaml'},
-    callback = function()
-        vim.bo.tabstop = 2
-        vim.bo.softtabstop = 2
-        vim.bo.shiftwidth = 2
-    end
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = { "css", "html", "javascript", "tex", "yaml" },
+	callback = function()
+		vim.bo.tabstop = 2
+		vim.bo.softtabstop = 2
+		vim.bo.shiftwidth = 2
+	end,
 })
 
-vim.api.nvim_create_autocmd('BufReadPost', {
-    pattern = '*',
-    callback = function()
-        if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
-            vim.cmd('normal! g`"')
-        end
-    end
+vim.api.nvim_create_autocmd("BufReadPost", {
+	pattern = "*",
+	callback = function()
+		if vim.fn.line("'\"") > 0 and vim.fn.line("'\"") <= vim.fn.line("$") then
+			vim.cmd('normal! g`"')
+		end
+	end,
 })
 
 -- Set undo options if version is 7.3 or above
 if vim.version().minor >= 3 then
-    vim.o.undodir = os.getenv("HOME") .. '/.vim/backup'
-    vim.o.undofile = true
-    vim.o.undoreload = 10000
+	vim.o.undodir = os.getenv("HOME") .. "/.vim/backup"
+	vim.o.undofile = true
+	vim.o.undoreload = 10000
 end
 
 vim.g.mapleader = " "
 
-vim.keymap.set({'n', 'v', 'i'}, '<M-Up>', '<Esc><C-w><Up>', {noremap = true})
-vim.keymap.set({'n', 'v', 'i'}, '<M-Down>', '<Esc><C-w><Down>', {noremap = true})
-vim.keymap.set({'n', 'v', 'i'}, '<M-Left>', '<Esc><C-w><Left>', {noremap = true})
-vim.keymap.set({'n', 'v', 'i'}, '<M-Right>', '<Esc><C-w><Right>', {noremap = true})
+vim.keymap.set({ "n", "v", "i" }, "<M-Up>", "<Esc><C-w><Up>", { noremap = true })
+vim.keymap.set({ "n", "v", "i" }, "<M-Down>", "<Esc><C-w><Down>", { noremap = true })
+vim.keymap.set({ "n", "v", "i" }, "<M-Left>", "<Esc><C-w><Left>", { noremap = true })
+vim.keymap.set({ "n", "v", "i" }, "<M-Right>", "<Esc><C-w><Right>", { noremap = true })
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable", -- latest stable release
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
-
